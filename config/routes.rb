04884fe4c10e543888071d1f43fcb36e2b6272a6
Cathella <ncathie214@gmail.com>
2021-05-23 14:48:50 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :projects do
+    resources :reports, module: :projects
+  end
+
+  root to: "projects#index"
 end
