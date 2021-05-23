d73db7f0afb348516009fb8f5c59ad82fe246cac
Cathella <ncathie214@gmail.com>
2021-05-23 22:35:00 +0300

    create project show view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,20 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, except: %i[ index create new ]
+
   def index
     @projects = Project.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Project.create(name: "Visual detection of common plant crop diseases using Deep learning, a proof of concept")
