e794c203765412addf658a848f2dafed7ab9c53d
Cathella <ncathie214@gmail.com>
2021-05-29 20:43:17 +0300

    send email on report creation

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,4 +2,15 @@ class Report < ApplicationRecord
   belongs_to :project
   has_many_attached :files
   validates :title, :description, :files, presence: true
+
+  # users
+
+
+  def send_notification!
+    users = ["werehenry@gmail.com", "ncathie214@gmail.com"]
+
+    users.each do |user|
+      NotificationMailer.report_notification(user, self).deliver_later
+    end
+  end
 end
