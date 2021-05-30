7e397c88324929636f44b60107e5e72f365ed16a
Cathella <ncathie214@gmail.com>
2021-05-30 08:42:26 +0300

    instructor auth

--- db/migrate/20210525074327_devise_create_users.rb
+++ db/migrate/20210525074327_devise_create_users.rb
@@ -32,7 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+      t.string :full_name
       t.timestamps null: false
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2021_05_30_045415) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "full_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_instructors_on_email", unique: true
@@ -87,6 +88,7 @@ ActiveRecord::Schema.define(version: 2021_05_30_045415) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "full_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
